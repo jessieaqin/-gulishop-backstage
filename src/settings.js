8c02b8456ccf358fe234f752319fdecb60b757c4
jessie <10477323+jessie66@user.noreply.gitee.com>
2022-06-09 22:21:25 +0800

    git init

---
+++ src/settings.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+  title: '谷粒商城后台管理',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+  // 项目nav是不是固定定位
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false
+  // 是否显示侧边栏的loge区域
+}
